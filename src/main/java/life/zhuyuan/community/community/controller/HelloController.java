8dd4d61421fa768ec4c81e936c1a3124cea9f013
unknown <1106808924@qq.com>
2020-09-25 16:28:58 +0800

    programmer

---
+++ src/main/java/life/zhuyuan/community/community/controller/HelloController.java
@@ -0,0 +1,18 @@
+package life.zhuyuan.community.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * Created by zhuzhiwen by 2020/9/25 9:50
+ */
+@Controller
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello(@RequestParam(name = "name") String name, Model model){
+        model.addAttribute("name",name);
+        return "hello";
+    }
+}
