71a5edf4a0991ed8b5d01deb2a8f5a5f7f3070f5
unknown <1106808924@qq.com>
2020-10-09 10:11:14 +0800

    add check cookie

--- src/main/java/life/zhiyuan/community/community/mapper/UserMapper.java
+++ src/main/java/life/zhiyuan/community/community/mapper/UserMapper.java
@@ -3,6 +3,8 @@ package life.zhiyuan.community.community.mapper;
 import life.zhiyuan.community.community.model.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 /**
  * Created by zhuzhiwen by 2020/10/8 20:38COMMUNTIY
@@ -11,4 +13,7 @@ import org.apache.ibatis.annotations.Mapper;
 public interface UserMapper {
     @Insert("insert into user (name,account_id,token,gmt_create,gmt_modified) values (#{name},#{accountId},#{token},#{gmtCreate},#{gmtModified})")
     void insert(User user);
+
+    @Select("select * from user where token = #{token}")
+    User findByToken(@Param("token") String token);
 }
